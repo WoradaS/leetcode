522444e5328616433fcc4f3908103919319bdce4
worada.sir <worada.sir@axonstech.com>
2024-03-08 15:37:19 +0700

    Remove Duplicates from Sorted Array 2

---
+++ Remove Duplicates from Sorted Array 2/index.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+    let index = 0
+    for(let i = 0; i < nums.length; i++){
+        if(nums[i] != nums[index-2]){
+            nums[index++] = nums[i]
+        }
+    }
+    return index
+};
